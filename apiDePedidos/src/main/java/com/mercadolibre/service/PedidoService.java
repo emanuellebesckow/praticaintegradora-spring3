f72e8bdcc3e4d4c8b76e7f528db9bcb42c219132
emanuellebesckow <emanuelle.besckow@mercadolivre.com>
2021-12-30 15:16:59 -0300

    métodos em organização

--- apiDePedidos/src/main/java/com/mercadolibre/service/PedidoService.java
+++ apiDePedidos/src/main/java/com/mercadolibre/service/PedidoService.java
@@ -4,7 +4,6 @@ import com.mercadolibre.model.Pedido;
 import com.mercadolibre.model.Prato;
 import com.mercadolibre.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,28 +12,29 @@ import java.util.concurrent.atomic.AtomicReference;
 @Service
 public class PedidoService {
 
-    //TODO - Colocar quantidade no calculo do total
+	// TODO - Colocar quantidade no calculo do total
 
-    @Autowired
-    PedidoRepository pedidoRepository;
+	@Autowired
+	PedidoRepository pedidoRepository;
 
-    private static int ID = 0;
+	private static int ID = 0;
 
-    public Pedido adicionarPratoAoPedido(List<Prato> listaPratos, int idMesa) {
-        AtomicReference<Double> totalPratos = new AtomicReference<>((double) 0); //referencia atomica para o stream
+	public Pedido adicionarPratoAoPedido(List<Prato> listaPratos, int idMesa) {
+		AtomicReference<Double> totalPratos = new AtomicReference<>((double) 0); // referencia atomica para o stream
 
-        listaPratos.stream().forEach(prato -> {
-            totalPratos.accumulateAndGet((prato.getPreco()*prato.getQuantidade()), Double::sum);
-        });
+		listaPratos.stream().forEach(prato -> {
+			totalPratos.accumulateAndGet((prato.getPreco() * prato.getQuantidade()), Double::sum);
+		});
 
-        ID ++;
-        Pedido pedido = Pedido.builder().listaPratos(listaPratos).idMesa(idMesa).id(ID).total(totalPratos.get()).build();
+		ID++;
+		Pedido pedido = Pedido.builder().listaPratos(listaPratos).idMesa(idMesa).id(ID).total(totalPratos.get())
+				.build();
 
-        pedidoRepository.createPedido(pedido);
-        return pedido;
-    }
+		pedidoRepository.createPedido(pedido);
+		return pedido;
+	}
 
-    public List<Pedido> findAll() {
-        return pedidoRepository.getPedido();
-    }
+	public List<Pedido> findAll() {
+		return pedidoRepository.getPedido();
+	}
 }
